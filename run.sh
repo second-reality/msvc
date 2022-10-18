ec20fc14876c8fc6e3596bc19ee6664154383871
Pierrick Bouvier <pierrick.bouvier@posteo.net>
2022-10-18 11:15:56 +0200

    run

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+script_dir=$(dirname $(readlink -f $0))
+
+docker build $script_dir/docker -t msvc
+
+tmp=$(mktemp -d)
+trap "rm -rf $tmp" EXIT
+
+all_id=$(id -G | tr ' ' '\n' | sed -e 's/^/--group-add /g' | tr '\n' ' ')
+docker run -it --rm=true -u $UID $all_id msvc ls
