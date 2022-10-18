5b2b3c5a3b3a85d9b85c61b463742348968f6a15
Pierrick Bouvier <pierrick.bouvier@posteo.net>
2022-10-18 11:36:38 +0200

    clean

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-set -x
 
 script_dir=$(dirname $(readlink -f $0))
 
 build_container()
 {
     docker build $script_dir/docker -t msvc
+    clear
 }
 
 create_wineprefix()
@@ -30,6 +30,8 @@ run_container()
     all_id=$(id -G | tr ' ' '\n' | sed -e 's/^/--group-add /g' | tr '\n' ' ')
     docker run -it --rm=true -u $UID $all_id \
         -v $wineprefix:$wineprefix \
+        -v $(pwd):$(pwd) \
+        -w $(pwd) \
         -e WINEPREFIX=$wineprefix \
         msvc "$@"
 }
