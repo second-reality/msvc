6b919c45f6545d63fc68d27d8ea00ddb3088ce23
Pierrick Bouvier <pierrick.bouvier@posteo.net>
2022-10-18 12:17:56 +0200

    wip

--- docker/vs_prompt.sh
+++ docker/vs_prompt.sh
@@ -8,10 +8,16 @@ die()
     exit 1
 }
 
-vs="C:/vs"
-
 arch=x64
 
-WINEPATH="$vs/vc/tools/msvc/14.33.31629/bin/Hostx64/$arch/;${WINEPATH}"
+vs_path="C:/vs"
+exe_cl_path="$vs_path/vc/tools/msvc/14.33.31629/bin/Hostx64/$arch"
+exe_rc_path="$vs_path/kits/10/bin/10.0.19041.0/$arch"
+libcmt_path="$vs_path/vc/tools/msvc/14.33.31629/lib/$arch/"
+
+WINEPATH="$exe_cl_path;${WINEPATH:-}"
+WINEPATH="$exe_rc_path;${WINEPATH:-}"
+LIB="$libcmt_path;${LIB:-}"
+LIBPATH="$LIB"
 
 "$@"
